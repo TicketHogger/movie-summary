397a2e18a0576515ea1d7efbb8faef20886f188b
Luan Tran <luan.phuoc.tran.96@gmail.com>
2018-11-15 17:04:07 -0800

    Wrote SQL insert script

--- code/database/index.js
+++ code/database/index.js
@@ -18,9 +18,11 @@ const Photo = sequelize.define('photo', {
     {
       type: Sequelize.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
+
     },
   photoUrl: Sequelize.STRING,
-});
+}, { timestamps: false });
 
 
 const Movie = sequelize.define('movie', {
@@ -28,23 +30,26 @@ const Movie = sequelize.define('movie', {
     {
       type: Sequelize.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
     },
   title: Sequelize.STRING,
-  score: Sequelize.INTEGER,
+  score: Sequelize.STRING,
   duration: Sequelize.STRING,
   rating: Sequelize.STRING,
   mainPhoto: Sequelize.STRING,
+  photo: Sequelize.STRING,
   genre: Sequelize.STRING,
   releaseDate: Sequelize.STRING,
-  synopsis: Sequelize.STRING,
-});
+  synopsis: Sequelize.TEXT,
+}, { timestamps: false });
 
 Photo.sync({ force: true });
 
-Movie.belongsTo(Photo, { foreignKey: 'fk_photo' });
+// Movie.belongsTo(Photo, { foreignKey: 'fk_photo' });
 
 Movie.sync({ force: true });
 
 
 module.Movie = Movie;
 module.Photo = Photo;
+
---
+++ code/seed-data/insert.sql
@@ -0,0 +1,6 @@
+-- COPY public.movies(title,score,duration, rating, mainPhoto, photos, genre, releaseDate, synopsis) 
+-- FROM '/Users/luantran/Documents/SDC/movie-summary/code/seed-data/summariesTest.csv' DELIMITER ',' CSV HEADER;
+
+COPY movies(title, score, duration, rating, "mainPhoto", photo, genre, "releaseDate", synopsis)
+FROM '/Users/luantran/Documents/SDC/movie-summary/code/seed-data/summariesTest.csv' DELIMITER ',' CSV HEADER;
+
